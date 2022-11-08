8fbdc5bbfe53aef88083a6d769eab9681e9e9e20
Максим Расторгуев <frontimax@yandex.ru>
2022-11-08 18:08:59 +0400

    Работа с файлом и получение погоды по API

--- weather.js
+++ weather.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
 import {getArgs} from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { getWeather } from './services/api.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+    if(!token.length) {
+        printError('Не передан token');
+        return;
+    }
+
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
+        printSuccess('Токе сохранён');
+    } catch (e) {
+        printError(e.message);
+    }
+};
 
 const initCLI = () => {
     const args = getArgs(process.argv);
@@ -11,8 +27,9 @@ const initCLI = () => {
 
     } 
     if (args.t) {
-
+       return saveToken(args.t);
     }
+    getWeather('Москва');
 };
 
 initCLI();
